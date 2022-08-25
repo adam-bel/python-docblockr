141d2b676467e018c69773849b5c192c311eb21f
thepoy <thepoy@163.com>
2022-08-25 17:48:22 +0800

    Revert "rename the package name"

--- utils/consts.py
+++ utils/consts.py
@@ -4,11 +4,11 @@
 # @Email:     thepoy@163.com
 # @File Name: consts.py
 # @Created:   2022-08-25 11:30:20
-# @Modified:  2022-08-25 17:46:36
+# @Modified:  2022-08-25 17:41:50
 
 LOG_FORMAT = "[%(levelname)s] %(asctime)s - %(name)s:%(lineno)d - %(message)s"
 
-PACKAGE_NAME = "PythonDocblockr"
+PACKAGE_NAME = "Python Docblockr"
 SETTING_FILE = "%s.sublime-settings" % PACKAGE_NAME
 
 TIME_FORMAT_WITH_DATE = "%Y-%m-%d %H:%M:%S"
