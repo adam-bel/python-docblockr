23df2f0c1676db0970f93a63b9f4651141b76d0b
thepoy <thepoy@163.com>
2022-08-26 09:47:41 +0800

    add file handler for logger

--- utils/log.py
+++ utils/log.py
@@ -4,15 +4,18 @@
 # @Email:     thepoy@163.com
 # @File Name: log.py
 # @Created:   2022-08-25 11:30:28
-# @Modified:  2022-08-25 17:29:47
+# @Modified:  2022-08-26 09:27:34
 
 import os
 import logging
 import sublime
 
-from typing import Dict, Optional, Literal
+from typing import Dict, Optional, Literal, Tuple
 from datetime import datetime
 from .consts import LOGGER_NAME, TIME_FORMAT_WITHOUT_DATE
+from .color import DisplayStyle, Style
+
+ds = DisplayStyle()
 
 _style = Literal["%", "{", "$"]
 
@@ -24,26 +27,36 @@ class Formatter(logging.Formatter):
         datefmt: Optional[str] = None,
         style: _style = "%",
         print_position=True,
+        to_file=False,
     ):
         self.default_color = "{0}"
         self.print_position = print_position
 
-        self.level_config: Dict[str, str] = {
-            "DEBUG": "DEB",
-            "INFO": "INF",
-            "WARN": "WAR",
-            "WARNING": "WAR",
-            "ERROR": "ERR",
-            "FATAL": "FAT",
-            "CRITICAL": "FAT",
+        self.level_config: Dict[str, Tuple[str, Style]] = {
+            "DEBUG": ("DEB", ds.fc.purple),
+            "INFO": ("INF", ds.fc.green),
+            "WARN": ("WAR", ds.fc.yellow),
+            "WARNING": ("WAR", ds.fc.yellow),
+            "ERROR": ("ERR", ds.fc.light_red),
+            "FATAL": ("FAT", ds.fc.red),
+            "CRITICAL": ("FAT", ds.fc.red),
         }
+
+        self.to_file = to_file
 
         super().__init__(fmt, datefmt, style)
 
     def __level(self, levelname: str):
         level = self.level_config[levelname]
 
-        return "[%s] " % level
+        if self.to_file:
+            return "%s%s%s " % (
+                ds.format_with_one_style("[", ds.fc.light_gray),
+                ds.format_with_one_style(level[0], level[1]),
+                ds.format_with_one_style("]", ds.fc.light_gray),
+            )
+
+        return "[%s] " % level[0]
 
     def __time(self, record):
         assert isinstance(self.datefmt, str)
@@ -55,9 +68,19 @@ class Formatter(logging.Formatter):
             else t.strftime(self.datefmt)
         )
 
+        if self.to_file:
+            return ds.format_with_one_style(s, ds.fc.dark_gray) + " "
+
         return s + " "
 
     def __name(self, record):
+        if self.to_file:
+            return (
+                ""
+                if record.name == "root"
+                else ds.format_with_one_style(record.name, ds.fc.cyan)
+            )
+
         return "" if record.name == "root" else f"{record.name}"
 
     def __position(self, record: logging.LogRecord):
@@ -67,10 +90,21 @@ class Formatter(logging.Formatter):
         if record.levelname in ["INFO"]:
             return ""
 
+        if self.to_file:
+            return (
+                ds.format_with_multiple_styles(
+                    f":{record.lineno}", ds.fc.light_yellow, ds.mode.bold
+                )
+                + " "
+            )
+
         return f":{record.lineno} "
 
     @property
     def __connector(self):
+        if self.to_file:
+            return ds.format_with_one_style("-", ds.fc.light_cyan) + " "
+
         return "- "
 
     def format(self, record: logging.LogRecord):
@@ -80,16 +114,14 @@ class Formatter(logging.Formatter):
 
         msg = record.msg % record.args if record.args else record.msg
 
-        s = (
-            self.__time(record)
-            + self.__level(record.levelname)
+        return (
+            self.__level(record.levelname)
+            + self.__time(record)
             + self.__name(record)
             + self.__position(record)
             + self.__connector
             + msg
         )
-
-        return s
 
 
 def log_level() -> int:
@@ -110,11 +142,44 @@ def log_level() -> int:
     )
 
 
+def stream_handler():
+    fmt = Formatter(datefmt=TIME_FORMAT_WITHOUT_DATE, print_position=True)
+    handler = logging.StreamHandler()
+    handler.setFormatter(fmt)
+
+    return handler
+
+
+def file_handler():
+    fmt = Formatter(datefmt=TIME_FORMAT_WITHOUT_DATE, print_position=True, to_file=True)
+
+    packages_path: str = sublime.packages_path()
+    sublime_config_path = os.path.dirname(packages_path)
+    log_dir_path = os.path.join(sublime_config_path, "Log")
+
+    if not os.path.exists(log_dir_path):
+        os.mkdir(log_dir_path)
+
+    handler = logging.FileHandler(
+        filename=os.path.join(log_dir_path, f"{LOGGER_NAME}.log"),
+        mode="w",
+        encoding="utf-8",
+    )
+    handler.setFormatter(fmt)
+
+    return handler
+
+
 def get_logger():
     """Get the root logger of the package.
 
     Use a custom Formatter to create a logger
     that is only valid for this package.
+
+    Since the sublime console does not support color printing,
+    for better debugging, I save the color log to a file.
+    When debugging, you can read the color log through
+    `tail -f` (unix-like) or `Get-Content -Wait -Path` (Windows).
 
     Returns:
         Logger: the instance of `logging.Logger`
@@ -122,13 +187,15 @@ def get_logger():
     # 如果不传入 name 则会返回 root logger，以后可通过 loggine.Logger 创建 logger 实例
     logger = logging.getLogger(LOGGER_NAME)
 
-    console_handler = logging.StreamHandler()
-    fmt = Formatter(datefmt=TIME_FORMAT_WITHOUT_DATE, print_position=True)
-    console_handler.setFormatter(fmt)
+    level = log_level()
 
-    logger.addHandler(console_handler)
+    if level == logging.DEBUG:
+        logger.addHandler(stream_handler())
+        logger.addHandler(file_handler())
+    else:
+        logger.addHandler(file_handler())
 
-    logger.setLevel(log_level())
+    logger.setLevel(level)
 
     return logger
 
